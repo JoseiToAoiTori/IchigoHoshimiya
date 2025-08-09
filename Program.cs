9226a73bb4d4a15f395e687c06dcb499c277f1fc
joseitoaoitori <acciowand200@hotmail.com>
2025-08-09 17:56:06 +0200

    we're getting somewhere

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.Extensions.Hosting;
-
+﻿using IchigoHoshimiya.Interfaces;
+using IchigoHoshimiya.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Services;
+using NetCord.Hosting.Services.ApplicationCommands;
 
-var builder = Host.CreateApplicationBuilder(args);
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSingleton<IPingService, PingService>();
 
 builder.Services
-    .AddDiscordGateway();
+       .AddDiscordGateway()
+       .AddApplicationCommands();
 
-var host = builder.Build();
+IHost host = builder.Build();
+
+// NetCord: Add commands from modules
+host.AddModules(typeof(Program).Assembly);
+
+
+// NetCord: Add handlers to handle the commands
+host.UseGatewayHandlers();
 
 await host.RunAsync();
