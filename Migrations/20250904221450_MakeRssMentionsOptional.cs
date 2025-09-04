b29e52756eca2e35e46cb99cd7409f0f1cd522e9
joseitoaoitori <acciowand200@hotmail.com>
2025-09-05 00:15:15 +0200

    missing migration

---
+++ Migrations/20250904221450_MakeRssMentionsOptional.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IchigoHoshimiya.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeRssMentionsOptional : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "mentions",
+                table: "rss_reminder",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "mentions",
+                table: "rss_reminder",
+                type: "longtext",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true);
+        }
+    }
+}
