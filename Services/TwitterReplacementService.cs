12031e39d548b92c94df41fc99d3b65c080ad54a
joseitoaoitori <acciowand200@hotmail.com>
2025-10-05 00:27:42 +0200

    fucking work already

--- Services/TwitterReplacementService.cs
+++ Services/TwitterReplacementService.cs
@@ -8,29 +8,32 @@ public partial class TwitterReplacementService : ITwitterReplacementService
     private const string ReplacementDomain = "https://girlcockx.com";
     private static readonly string[] SDomainsToWatch = ["https://twitter.com", "https://x.com"];
 
-    private static readonly Regex SFxTwitterRegex = MyRegex();
-
+    private static readonly Regex STweetLinkRegex = TweetLinkRegex();
 
     public Task<string?> GetReplacedContentAsync(string originalContent, string username)
     {
         var hasLink = SDomainsToWatch.Any(originalContent.Contains);
-
         if (!hasLink)
         {
             return Task.FromResult<string?>(null);
         }
-
-        var content = SDomainsToWatch.Aggregate(
+        
+        var content = STweetLinkRegex.Replace(
             originalContent,
-            (current, domain) => current.Replace(domain, ReplacementDomain));
+            match =>
+            {
+                var replaced = match.Value
+                                    .Replace("https://twitter.com", ReplacementDomain)
+                                    .Replace("https://x.com", ReplacementDomain);
 
-        content = SFxTwitterRegex.Replace(content, "$1/en");
+                return $"{replaced}/en";
+            });
 
         return content != originalContent
             ? Task.FromResult<string?>($"Sent by {username}: {content}")
             : Task.FromResult<string?>(null);
     }
-
-    [GeneratedRegex(@"(https://girlcockx\.com/[^/\s]+/status/[^\s?|]+)", RegexOptions.Compiled)]
-    private static partial Regex MyRegex();
+    
+    [GeneratedRegex(@"https://(?:twitter\.com|x\.com)/[^/\s]+/status/[^\s?|]+", RegexOptions.Compiled)]
+    private static partial Regex TweetLinkRegex();
 }
